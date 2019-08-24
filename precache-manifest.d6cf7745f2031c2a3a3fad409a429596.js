9f41a27e34ea9e88367b280e5620a018cd536f73
zhuangliuting <3160102518@zju.edu.cn>
2019-08-24 22:47:19 +0800

    first commit

---
+++ precache-manifest.d6cf7745f2031c2a3a3fad409a429596.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d092a5bde28bda9f97c1280fa3795bd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eeb4e218cf8abcc47b1",
+    "url": "/static/css/2.1d6907fe.chunk.css"
+  },
+  {
+    "revision": "abe75b40e2e5f5c8259e",
+    "url": "/static/css/main.8fa1f4f7.chunk.css"
+  },
+  {
+    "revision": "3eeb4e218cf8abcc47b1",
+    "url": "/static/js/2.71c2cdf7.chunk.js"
+  },
+  {
+    "revision": "abe75b40e2e5f5c8259e",
+    "url": "/static/js/main.bbbe2fe1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
